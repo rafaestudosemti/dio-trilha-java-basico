c309b9d13ce0a54be287a915d3fef13a8ae9d05c
Rafael Em TI <rafaestudosemti@gmail.com>
2023-11-25 11:57:48 -0300

    new file:   sistema-smart-tv/.vscode/settings.json 	new file:   sistema-smart-tv/README.md 	new file:   sistema-smart-tv/src/SmartTv.java 	new file:   sistema-smart-tv/src/Usuario.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        //Constante - são variaveis que não mudam
+        final double VALOR_DE_PI = 3.14;
+
+        //Operador ternário
+        int a, b;
+        a = 5;
+        b = 6;
+        String resultado = a==b ?"verdadeiro" : "falso";
+        System.out.println(resultado);
+        
     }
 }
